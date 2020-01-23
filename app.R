41abd535016066a7c11a3dd341b9f6e6ba3ff006
Aep Hidyatuloh <aef.stk@gmail.com>
2020-01-23 19:00:59 +0700

    Fix bs4ard height

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(shinyalert)
 library(bs4Dash)
-library(DT)
 library(plotly)
 library(readxl)
 library(shinyWidgets)
@@ -16,27 +15,19 @@ ui <- bs4DashPage(navbar = bs4DashNavbar(skin = "light", status = "white",
                     br(),
                     fluidRow(
                       column(3,
-                             bs4Card(width = 12, title = "Data", status = "primary", closable = FALSE,
+                             bs4Card(width = 12, title = "Data", status = "primary", closable = FALSE, collapsible = FALSE,
                                      fileInput("data", "Upload Data", width = "100%",
                                                accept = c('.xls', '.xlsx')
                                                ),
-                                     column(12, 
-                                            # fluidRow(
-                                                     awesomeCheckbox(inputId = "firstrow", label = "First row as column name", value = TRUE, width = "100%"),
-                                                     awesomeCheckbox(inputId = "firstcol", label = "First column as Respondent ID", value = TRUE, width = "100%")
-                                            # )
-                                     ),
                                      column(12,
                                             uiOutput("sheets")
-                                     ),
-                                     actionBttn(inputId = "submit", label = "Process", block = TRUE,
-                                                style = "simple", color = "primary", icon = icon("cogs"))
+                                     )
                                      )
                              ),
-                      column(2,
+                      column(3,
                              uiOutput("index")
                              ),
-                      column(7,
+                      column(6,
                              uiOutput("quadrant")
                              )
                       
@@ -86,9 +77,14 @@ server <- function(input, output, session){
     output$sheets <- renderUI({
       if(!is.null(input$data)){
         tagList(
-          selectInput("performance", "Performance Sheet", choices = excel_sheets(input$data$datapath), selected = excel_sheets(input$data$datapath)[2]),
-          selectInput("importance", "Importance Sheet", choices = excel_sheets(input$data$datapath), selected = excel_sheets(input$data$datapath)[1])
-        )
+                awesomeCheckbox(inputId = "firstrow", label = "First row as column name", value = TRUE, width = "100%"),
+                awesomeCheckbox(inputId = "firstcol", label = "First column as Respondent ID", value = TRUE, width = "100%"),
+                selectInput("performance", "Performance Sheet", choices = excel_sheets(input$data$datapath), selected = excel_sheets(input$data$datapath)[2]),
+                selectInput("importance", "Importance Sheet", choices = excel_sheets(input$data$datapath), selected = excel_sheets(input$data$datapath)[1]),
+                actionBttn(inputId = "submit", label = "Process", block = TRUE,
+                           style = "simple", color = "primary", icon = icon("cogs")),
+                br()
+                )
       } else {
         return(NULL)
       }
@@ -99,17 +95,20 @@ server <- function(input, output, session){
     output$index <- renderUI({
       if(!is.null(ic())){
         avg <- ic()$Average
-        tagList(
-          bs4ValueBox(value = h3(round(avg$Performance, 2), style = "font-weight:bold;"), 
-                     subtitle = "Performance Index", width = 12, 
-                     status = "primary", icon = "chart-line"),
-          bs4ValueBox(value = h3(round(avg$Importance, 2), style = "font-weight:bold;"), 
-                     subtitle = "Importance Index", width = 12, 
-                     status = "success", icon = "check-double"),
-          bs4ValueBox(value = h3(paste0(round(avg$Conformity*100, 2), "%"), style = "font-weight:bold;"), 
-                     subtitle = "Conformity", width = 12, 
-                     status = "info", icon = "thumbs-up")
-          
+        bs4Card(width = 12, height = 485, title = "Index", status = "primary", closable = FALSE, collapsible = FALSE,
+                br(),
+                bs4InfoBox(value = h3(round(avg$Performance, 2), style = "font-weight:bold;"), 
+                           title = "Performance Index", width = 12, 
+                           status = "primary", icon = "chart-line"),
+                br(),
+                bs4InfoBox(value = h3(round(avg$Importance, 2), style = "font-weight:bold;"), 
+                           title = "Importance Index", width = 12, 
+                           status = "success", icon = "check-double"),
+                br(),
+                bs4InfoBox(value = h3(paste0(round(avg$Conformity*100, 2), "%"), style = "font-weight:bold;"), 
+                           title = "Conformity", width = 12, 
+                           status = "info", icon = "thumbs-up"),
+                br()
         )
       } else {
         return(NULL)
@@ -117,36 +116,6 @@ server <- function(input, output, session){
     })
   })
   
-  
-  # output$perf <- renderInfoBox({
-  #   req(input$data)
-  #   avg <- ic()$Average
-  #   perf <- avg$Performance
-  #   # avg
-  #   bs4InfoBox(value = h3(round(perf, 2), style = "font-weight:bold;"), 
-  #              title = "Performance Index", width = 4, 
-  #              status = "primary", icon = "chart-line")
-  # })
-  # 
-  # output$imp <- renderInfoBox({
-  #   req(input$data)
-  #   avg <- ic()$Average
-  #   imp <- avg$Importance
-  #   # avg
-  #   bs4InfoBox(value = h3(round(imp, 2), style = "font-weight:bold;"), 
-  #              title = "Importance Index", width = 4, 
-  #              status = "success", icon = "check-double")
-  # })
-  # 
-  # output$conf <- renderInfoBox({
-  #   req(input$data)
-  #   avg <- ic()$Average
-  #   conf <- avg$Conformity
-  #   # avg
-  #   bs4InfoBox(value = h3(paste0(round(conf*100, 2), "%"), style = "font-weight:bold;"), 
-  #              title = "Conformity", width = 4, 
-  #              status = "info", icon = "thumbs-up")
-  # })
   
   plots <- reactive({
     if(is.null(ic())){
@@ -172,7 +141,7 @@ server <- function(input, output, session){
   observeEvent(input$submit, {
     output$quadrant <- renderUI({
       if(!is.null(ic())){
-        bs4Card(width = 12, title = "Quadrant", status = "primary", closable = FALSE,
+        bs4Card(width = 12, height = 485, title = "Quadrant", status = "primary", closable = FALSE, collapsible = FALSE,
                 uiOutput("savebtn"),
                 plotlyOutput("plot")
         )
